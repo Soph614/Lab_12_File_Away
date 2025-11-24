e260b6e5b3cb79e0bd6a1e90854a3db86b8c3371
soph <sophia@javamate.net>
2025-11-24 11:13:14 -0500

    Commit 5

--- src/TestCode.java
+++ src/TestCode.java
@@ -2,11 +2,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestCode {
     public static void main(String[] args) throws IOException {
+        final int FIELDS_LENGTH = 5;
         String firstName, lastName, idNumber, eMail, yearOfBirth;
         File file1 = new File("userData.txt");
         FileWriter fileWriter = new FileWriter(file1);
@@ -17,23 +19,31 @@ public class TestCode {
         int timesGoneThrough = 0;
         do {
             firstName = SafeInput.getNonZeroLenString(pipe, "Your first name: ");
-            printWriter.write(firstName);
+            printWriter.write(firstName + ", ");
             lastName = SafeInput.getNonZeroLenString(pipe, "Your last name: ");
-            printWriter.write(lastName);
+            printWriter.write(lastName + ", ");
             idNumber = SafeInput.getRegExString(pipe, "Enter your ID number: ", "[1-9]");
             idNumber = 0 + idNumber;
             idNumber = 0 + idNumber;
             idNumber = 0 + idNumber;
             idNumber = 0 + idNumber;
             idNumber = 0 + idNumber;
-            printWriter.write(idNumber);
+            printWriter.write(idNumber + ", ");
             eMail = SafeInput.getRegExString(pipe, "Enter your e-mail: ", "[a-zA-Z0-9\\.]+@[a-zA-Z0-9\\.]+");
-            printWriter.write(eMail);
+            printWriter.write(eMail + ", ");
             yearOfBirth = SafeInput.getRegExString(pipe, "Enter your year of birth: ", "^\\d{4}$");
             printWriter.write(yearOfBirth);
+            printWriter.println();
             printWriter.close();
             timesGoneThrough++;
             done = SafeInput.getYNConfirm(pipe, "Are you done?");
         } while (!done);
+        Path file = file1.toPath();
+        String [] fields;
+
+
+        for (int l = 0; l < timesGoneThrough; l++)
+        {
+        }
     }
 }
